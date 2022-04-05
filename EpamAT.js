e3b0075b3ba6d1a6fc890c65255388663176df56
Vasilevski.as <vasilevski.as@gmail.com>
2022-04-05 19:38:16 +0300

    addition, multiplication, division, transformation

---
+++ EpamAT.js
@@ -0,0 +1,23 @@
+// 1 сложение различных типов
+
+console.log("string" + true);
+console.log("string" + 4);
+console.log(5 + true);
+
+// 2 умножение различных типов
+
+console.log("string" * true);
+console.log("string" * 4);
+console.log(5 * true);
+
+// 3 деление различных типов
+
+console.log("string" / true);
+console.log("string" / 4);
+console.log(5 / true);
+
+// 4 явное преобразование
+
+console.log(Number("20"));
+console.log(String(false));
+console.log(Boolean(1));
